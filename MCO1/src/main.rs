1d39cbc3e78e707e4bd57bdcfdb5e81db71cb065
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-10-23 20:06:06 +0800

    MCO1: Add Currency, input(), and register()

--- MCO1/src/main.rs
+++ MCO1/src/main.rs
@@ -1,15 +1,28 @@
 use std::io;
 use std::io::Write;
 
+enum Currency {
+    PHP,
+    USD,
+    JPY,
+    GBP,
+    EUR,
+    CNY
+}
+
 fn main() {
     let mut is_running = true;
-    let mut input = String::new();
+    let mut user_input = String::new();
 
-    println!("Welcome to MCO1 Banking and Currency App made with Rust!\n");
+    let mut name: String = String::new();
+    let mut currency: Currency = Currency::PHP;
+    let mut balance: f64 = 0.0;
+
+    println!("Welcome to MCO1 Banking and Currency App made with Rust!");
 
     // Main loop
     while is_running {
-        print!("Main Menu
+        user_input = input("\nMain Menu
 [0] Exit
 [1] Register Account Name
 [2] Deposit Amount
@@ -17,18 +30,44 @@ fn main() {
 [4] Currency Exchange
 [5] Record Exchange Rates
 [6] Show Interest Amount
-Select Transaction: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut input).expect("ERROR: Input failed.");
-
-        if input.trim() == "0" {
+Select Transaction");
+        
+        if user_input.trim() == "0" {
             is_running = false;
+        } else if user_input.trim() == "1" {
+            register(&name);
+        } else if user_input.trim() == "2" {
+
+        } else if user_input.trim() == "3" {
+
+        } else if user_input.trim() == "4" {
+
+        } else if user_input.trim() == "5" {
+
+        } else if user_input.trim() == "6" {
+
         } else {
-            println!("ERROR: Incorrect input.\n");
+            println!("ERROR: Incorrect user_input.");
         }
 
-        input.clear();
+        user_input.clear();
     }
 
     print!("\nThank you and goodbye!");
+}
+
+fn input(prompt: &str) -> String {
+    let mut user_input = String::new();
+    
+    print!("{}: ", prompt);
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input).expect("ERROR: Input failed.");
+
+    user_input
+}
+
+fn register(name: &String) {
+    //name = input("Register Account Name
+//Account Name");
+    input("Back to the Main Menu (Y/N)");
 }

7fe0d60ad8abc405af9c00cf3a48c54cd3bbfd75
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-10-24 10:25:11 +0800

    MCO1: Complete update_exchange_rate()

--- MCO1/src/main.rs
+++ MCO1/src/main.rs
@@ -24,6 +24,18 @@ impl Currency {
             Currency::CNY => "CNY",
         }
     }
+
+    fn index(index: usize) -> Option<Currency> {
+        match index {
+            0 => Some(Currency::PHP),
+            1 => Some(Currency::USD),
+            2 => Some(Currency::JPY),
+            3 => Some(Currency::GBP),
+            4 => Some(Currency::EUR),
+            5 => Some(Currency::CNY),
+            _ => None
+        }
+    }
 }
 
 impl fmt::Display for Currency {
@@ -71,9 +83,9 @@ Select Transaction");
         } else if user_input == "3" && name != "" && balance > 0.0 {
             withdraw(&name, &mut balance, &currency);
         } else if user_input == "4" && name != "" {
-            update_exchange_rate(&mut exchange_rate);
+            
         } else if user_input == "5" && name != "" {
-
+            update_exchange_rate(&mut exchange_rate);
         } else if user_input == "6" && name != "" {
 
         } else if name == "" {
@@ -120,27 +132,24 @@ fn register(name: &mut String) {
     let mut user_input: String;
 
     while !is_finished {
-        if !is_name_valid {
-            user_input = input("\nRegister Account Name
+        user_input = input("\nRegister Account Name
 Account Name");
 
-            for c in user_input.chars() {
-                if !c.is_alphabetic() && c != ' ' {
-                    is_name_valid = false;
-                    break;
-                } else if !is_name_valid {
-                    is_name_valid = true;
-                }
+        for c in user_input.chars() {
+            if !c.is_alphabetic() && c != ' ' {
+                is_name_valid = false;
+                break;
+            } else if !is_name_valid {
+                is_name_valid = true;
             }
+        }
 
-            if !is_name_valid {
-                println!("ERROR: Name not valid. Alphabets and spaces are allowed.");
-                continue;
-            }
-            
-            *name = user_input;
+        if !is_name_valid {
+            println!("ERROR: Input not valid. Alphabets and spaces are allowed.");
+            continue;
         }
         
+        *name = user_input;
         is_finished = prompt();
         is_name_valid = false;
     }
@@ -148,85 +157,77 @@ Account Name");
 
 fn deposit(name: &String, balance: &mut f64, currency: &Currency) {
     let mut is_finished: bool = false;
-    let mut is_deposit_valid: bool = false;
     let mut user_input: String;
     let mut parsed_user_input: f64;
 
     while !is_finished {
-        if !is_deposit_valid {
-            println!("\nWithdraw Amount
+        println!("\nWithdraw Amount
 Account Name: {name}
 Current Balance: {balance}
 Currency: {}", currency.to_str());
-            user_input = input("Deposit Amount");
-            
-            if !user_input.parse::<f64>().is_ok() {
-                println!("ERROR: Input not valid.");
-                continue;
-            }
-
-            parsed_user_input = user_input.parse::<f64>().unwrap();
-
-            if parsed_user_input <= 0.0 {
-                println!("ERROR: Deposit must be greater than 0.");
-                continue;
-            } else if parsed_user_input + *balance > 1_000_000.0 {
-                println!("ERROR: Balance must be less than or equal to 1,000,000 after depositing.");
-                continue;
-            }
-
-            *balance += user_input.parse::<f64>().unwrap();
-            println!("Updated Balance: {}", balance);
+        user_input = input("Deposit Amount");
+        
+        if !user_input.parse::<f64>().is_ok() {
+            println!("ERROR: Input not valid.");
+            continue;
         }
+
+        parsed_user_input = user_input.parse::<f64>().unwrap();
+
+        if parsed_user_input <= 0.0 {
+            println!("ERROR: Deposit must be greater than 0.");
+            continue;
+        } else if parsed_user_input + *balance > 1_000_000.0 {
+            println!("ERROR: Balance must be less than or equal to 1,000,000 after depositing.");
+            continue;
+        }
+
+        *balance += user_input.parse::<f64>().unwrap();
+        println!("Updated Balance: {}", balance);
 
         if *balance == 1_000_000.0 {
             break;
         }
 
         is_finished = prompt();
-        is_deposit_valid = false;
     }
 }
 
 fn withdraw(name: &String, balance: &mut f64, currency: &Currency) {
     let mut is_finished: bool = false;
-    let mut is_deposit_valid: bool = false;
     let mut user_input: String;
     let mut parsed_user_input: f64;
 
     while !is_finished {
-        if !is_deposit_valid {
-            println!("\nDeposit Amount
+        println!("\nDeposit Amount
 Account Name: {name}
 Current Balance: {balance}
 Currency: {}", currency.to_str());
-            user_input = input("Deposit Amount");
-            
-            if !user_input.parse::<f64>().is_ok() {
-                println!("ERROR: Input not valid.");
-                continue;
-            }
-
-            parsed_user_input = user_input.parse::<f64>().unwrap();
-
-            if parsed_user_input <= 0.0 {
-                println!("ERROR: Withdraw must be greater than 0.");
-                continue;
-            } else if *balance - parsed_user_input < 0.0 {
-                println!("ERROR: Balance must be at least 0 after withdrawing.");
-                continue;
-            }
-
-            *balance -= user_input.parse::<f64>().unwrap();
-            println!("Updated Balance: {}", balance);
+        user_input = input("Deposit Amount");
+        
+        if !user_input.parse::<f64>().is_ok() {
+            println!("ERROR: Input not valid.");
+            continue;
         }
+
+        parsed_user_input = user_input.parse::<f64>().unwrap();
+
+        if parsed_user_input <= 0.0 {
+            println!("ERROR: Withdraw must be greater than 0.");
+            continue;
+        } else if *balance - parsed_user_input < 0.0 {
+            println!("ERROR: Balance must be at least 0 after withdrawing.");
+            continue;
+        }
+
+        *balance -= user_input.parse::<f64>().unwrap();
+        println!("Updated Balance: {}", balance);
 
         if *balance == 0.0 {
             break;
         }
 
         is_finished = prompt();
-        is_deposit_valid = false;
     }
 }
 
@@ -234,11 +235,13 @@ fn update_exchange_rate(exchange_rate: &mut HashMap<Currency, f64>) {
     let mut is_finished: bool = false;
     let mut is_currency_valid: bool = false;
     let mut user_input: String;
-    let mut currency: i8;
+    let mut currency_index: usize;
+    let mut currency: Currency = Currency::PHP;
+    let mut value: f64;
 
     while !is_finished {
         if !is_currency_valid {
-            user_input = input("Record Exchange Rate
+            user_input = input("\nRecord Exchange Rate
 [1] United States Dollar (USD)
 [2] Japanese Yen (JPY)
 [3] British Pound Sterling (GBP)
@@ -246,20 +249,39 @@ fn update_exchange_rate(exchange_rate: &mut HashMap<Currency, f64>) {
 [5] Chinese Yuan Renminni (CNY)
 Select Foreign Currency");
 
-            if !user_input.parse::<i8>().is_ok() {
+            if !user_input.parse::<usize>().is_ok() {
                 println!("ERROR: Input not valid.");
                 continue;
             }
 
-            currency = user_input.parse::<i8>().unwrap();
+            currency_index = user_input.parse::<usize>().unwrap();
 
-            if currency < 1 || currency > 5 {
+            if let Some(c) = Currency::index(currency_index) {
+                currency = c;
+                is_currency_valid = true;
+            } else {
                 println!("ERROR: Input not valid.");
                 continue;
             }
         }
 
+        user_input = input("Exchange Rate");
+
+        if !user_input.parse::<f64>().is_ok() {
+            println!("ERROR: Input not valid.");
+            continue;
+        }
+
+        value = user_input.parse::<f64>().unwrap();
+
+        if value <= 0.0 || value > 100_000.0 {
+            println!("ERROR: Exchange rate must be greater than 0 and less than 100,000.");
+            continue;
+        }
+
+        exchange_rate.insert(currency, value);
         is_finished = prompt();
+        currency = Currency::PHP;
         is_currency_valid = false;
     }
 }

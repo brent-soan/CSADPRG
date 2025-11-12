109c53d5d8e17e2be5c067798627c89ba4622ed0
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-11-12 10:23:58 +0800

    MCO2: Update main.rs

--- MCO2/src/main.rs
+++ MCO2/src/main.rs
@@ -1,3 +1,8 @@
+use polars::prelude::*;
+
 fn main() {
-    
+    let lf = LazyCsvReader::new(PlPath::new("dpwh_flood_control_projects.csv"))
+        .with_has_header(true)
+        .finish()?;
+    //println!("{:?}", lf);
 }
